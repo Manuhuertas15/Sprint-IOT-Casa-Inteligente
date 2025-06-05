b980055ccbc38749a395d310b97d0e4694ccd6e1
Manuella Huertas Bettini <huertas.manu08@gmail.com>
2025-06-05 08:39:16 -0300

    Adicionei código PROJETO ESOLA INTELIGENTE.

--- PROJETO ESCOLA INTELIGENTE/PROJETO ESCOLA INTELIGENTE.ino
+++ PROJETO ESCOLA INTELIGENTE/PROJETO ESCOLA INTELIGENTE.ino
@@ -1,40 +1,78 @@
-const int PIR = 2;
-const int ledVerde = 12;
-const int buzzer = 10;
-const int MQ135 = A0; //MQ = sensor de gás 
+//bibliotecas
+#include <DHT11.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-void ledPresenca(){
-  int estadoPIR = digitalRead(PIR);// Ler pino digital 2
+#define DHTPINO A2 //Definimos qual sera o pino para o sensor
+#define DHTTYPE DHT11 // variaveis do tipo DHT
 
-  // HIGH = 1
-  // LOW = 0
-  if (estadoPIR == HIGH) {
-    digitalWrite(ledVerde, HIGH);
-  }
-  else {
-    digitalWrite(ledVerde, LOW);
-  }
-  //delay(1000);
+DHT dht(DHTPINO, DHT11 ); 
+
+const int PIR = 2;  
+const int ledAmarelo = 13;
+
+const int MQ135 = A0; 
+const int buzzer = 12;
+
+void alarme_dois_tons() {
+  int freqAlta = 2000;
+  int freqBaixa = 800;
+  int duracaoTom = 250;
+
+  tone(buzzer, freqAlta, duracaoTom);
+  delay(duracaoTom);
+  tone(buzzer, freqBaixa, duracaoTom);
+  delay(duracaoTom);
 }
 
-void gasConcentrado(){
+
+void acenderLEDAoDetectarPresenca(){
+  int estadoPIR = digitalRead(PIR); // le o pino Digital 2
+
+  if (estadoPIR == HIGH){
+
+    digitalWrite(ledAmarelo, HIGH);// fala ao programa que para pino 13 "acender"
+    Serial.println("LED ligado");
+  } 
+ else {
+    digitalWrite(ledAmarelo, LOW);// fala ao programa que para pino 13 "acender"
+    Serial.println("LED apagado");
+  }
+}
+
+void verificarVazamentoDeGas() {
   int estadoMQ135 = analogRead(MQ135);
+
+  //if verificar a intensidade do valor do esatdoMQ135 >= 600
+  //sim - ativar alarme
+  //não - desativar alarme
+
   Serial.println(estadoMQ135);
-  if (estadoMQ135 >= 500) {
-    digitalWrite(buzzer, HIGH);
+
+  if (estadoMQ135 >= 400) {
+    alarme_dois_tons();
+  } else {
+    noTone(buzzer);
   }
-  else {
-    digitalWrite(buzzer, LOW);
-  }
+}
+
+
+
+void vericarTemperaturaEUmidade(){
+ float umidade = dht.readHumidity();
+ float temperatura = dht.readTemperature();
+ 
+ Serial.println("umidade" + String (umidade) + "%");
+ Serial.println("temperatura: " + String (temperatura) + "C");
 }
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(ledVerde, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
 
   pinMode(MQ135, INPUT);
-  
+
   pinMode(buzzer, OUTPUT);
 
   Serial.println("Calibrando os sensores");
@@ -55,7 +93,9 @@ void setup() {
 }
 
 void loop() {
-  // Somente chamada de funcoes
-  ledPresenca();
-  gasConcentrado();
+// As insruções no loop será somente chamada de funções
+  acenderLEDAoDetectarPresenca();
+  verificarVazamentoDeGas();
+  alarme_dois_tons();
+  vericarTemperaturaEUmidade();
 }
